340ef05ebb8556d7dad0f77533023ce94175271f
Alex Cessinas <alexcessinas@gmail.com>
2020-02-19 12:23:20 +0100

    correction bug onload

--- js/script.js
+++ js/script.js
@@ -1,96 +1,87 @@
-
-var tabChoix = new Array('r','v','b','j','o','f');
-
-var tirage = new Array();
+var tabChoix = ['r', 'v', 'b', 'j', 'o', 'f'];
+var tirage = [];
 var essai = 0;
 var proposition = new Array(0);
 var okPosition = 0;
 var okCouleur = 0;
 var test = true;
 var fini = true;
+var nom_image = "logo/images/mm_";
+var ext = ".gif";
+
+
 // génère une ligne aléatoirement
-function couleurAlea(){
-	for (int i = 0 ; i < 4 ; i++){
-		var nombre_aleatoire =Math.random();
-		var index = Math.round(5*nombre_aleatoire);
-		tirage[i]=tabChoix[index];
-	}
+function couleurAlea() {
+  for ( i = 0; i < 4; i++ ) {
+    var nombre_aleatoire = Math.random();
+    var index = Math.round(5 * nombre_aleatoire);
+    tirage[i] = tabChoix[index];
+  }
 }
 // Reproduire 4 fois pour récupérer la couleur d'une ligne
-function choixJoueur(color){
-	if (length(proposition)>4){
-		alert("Vous avez choisi 4 couleurs effacer ou valider pour continuer");
-	}
-	else{
-		proposition[i]=color;
-	}
+function choixJoueur(color) {
+  if (length(proposition) < 4) {
+    alert("Vous avez choisi 4 couleurs effacer ou valider pour continuer");
+  } else {
+    proposition[i] = color;
+  }
 }
 // vide la ligne de l'utilisateur
-function clearProposition(){
-	for(int i =  0; i < ''; i++)
-	{
-		proposition[i]='';
-	}
+function clearProposition() {
+  for (let i = 0; i < 4; i++) {
+    proposition[i] = '';
+  }
 }
 // compare la ligne généré avec la ligne de l'utilisateur
-funtion comparaison(){
-	for (int i = 0; i < 4; i++){
-		for (int j = 0; i < 4;i++){
-			if (tirage[i]===proposition[j]){
-				if (i===j){
-					okPosition +=1;
-				}
-				else{
-					okCouleur +=1;
-				}
-			}
-		}
-	}
-	clearProposition
+function comparaison() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; i < 4; i++) {
+      if (tirage[i] === proposition[j]) {
+        if (i === j) {
+          okPosition += 1;
+        } else {
+          okCouleur += 1;
+        }
+      }
+    }
+  }
+  clearProposition
 }
 
-function partie(){
-	while(fini || essai<10){
-			jeu;
-	}
-	if (okPosition===4) {
-		alert("Vous avez gagner en "+essai+" !");
-	}
-	else{
-		alert("Vous avez perdu le code caché était "+tabChoix[0]+"|"+tabChoix[1]+"|"+tabChoix[2]+"|"+tabChoix[3]);
-	}
+function partie() {
+  while (fini || essai < 10) {
+    jeu;
+  }
+  if (okPosition === 4) {
+    alert("Vous avez gagner en " + essai + " !");
+  } else {
+    alert("Vous avez perdu le code caché était " + tabChoix[0] + "|" + tabChoix[1] + "|" + tabChoix[2] + "|" + tabChoix[3]);
+  }
 }
 // définir effacer,selection,valider dans le html
-function jeu(){
-	while(test){
-		if (effacer){
-			clearProposition;
-		}
-		if (selection){
-			choixJoueur(color);
-		}
-		if (valider){
-			comparaison;
-			essai +=1;
-			test=false;
-			if (okPosition===4){
-				fini=false
-			}
-		}
-	}
+function jeu() {
+  while (test) {
+    if (effacer) {
+      clearProposition;
+    }
+    if (selection) {
+      choixJoueur(color);
+    }
+    if (valider) {
+      comparaison;
+      essai += 1;
+      test = false;
+      if (okPosition === 4) {
+        fini = false
+      }
+    }
+  }
 }
-<<<<<<< HEAD
-=======
 
 function solution() {
-
-// On marque la partie comme terminee
-    fini = true;
-
-// On affiche les images de la solution
-    document.tirage0.src = nom_image + tirage[0] + ".gif";
-    document.tirage1.src = nom_image + tirage[1] + ".gif";
-    document.tirage2.src = nom_image + tirage[2] + ".gif";
-    document.tirage3.src = nom_image + tirage[3] + ".gif";
-    }
->>>>>>> 3564b78b131237afbe35db4a41657ab4d5fc801b
+  fini = true;
+  document.tirage0.src = nom_image + tirage[0] + ".gif";
+  document.tirage1.src = nom_image + tirage[1] + ".gif";
+  document.tirage2.src = nom_image + tirage[2] + ".gif";
+  document.tirage3.src = nom_image + tirage[3] + ".gif";
+}
